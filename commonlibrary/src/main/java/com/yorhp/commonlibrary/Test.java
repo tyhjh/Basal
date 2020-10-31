25d4f09829faa40c06d157221f5175e0c2b5e2bc
Tyhj <tyhj5@qq.com>
2020-10-31 16:16:56 +0800

    第一个版本

---
+++ commonlibrary/src/main/java/com/yorhp/commonlibrary/Test.java
@@ -0,0 +1,10 @@
+package com.yorhp.commonlibrary;
+
+/**
+ * @author tyhj
+ * @date 2020/10/29
+ * @Description: java类作用描述
+ */
+
+class Test {
+}
---
+++ commonlibrary/src/main/java/com/yorhp/commonlibrary/accessbility/BaseAccessbilityService.java
@@ -0,0 +1,287 @@
+package com.yorhp.commonlibrary.accessbility;
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.GestureDescription;
+import android.annotation.TargetApi;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.graphics.Path;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.List;
+
+/**
+ * @author Tyhj
+ * @date 2019/6/30
+ */
+
+public abstract class BaseAccessbilityService extends AccessibilityService {
+
+    /**
+     * 模拟触摸事件
+     *
+     * @param x
+     * @param y
+     * @param duration
+     * @param callback
+     */
+    protected boolean clickOnScreen(float x, float y, int duration, GestureResultCallback callback) {
+        Path path = new Path();
+        path.moveTo(x, y);
+        return gestureOnScreen(path, 0, duration, callback);
+    }
+
+    /**
+     * 模拟触摸
+     *
+     * @param path
+     * @param startTime
+     * @param duration
+     * @param callback
+     */
+    protected boolean gestureOnScreen(Path path, long startTime, long duration,
+                                      GestureResultCallback callback) {
+        GestureDescription.Builder builde = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            builde = new GestureDescription.Builder();
+            builde.addStroke(new GestureDescription.StrokeDescription(path, startTime, duration));
+            GestureDescription gestureDescription = builde.build();
+            return dispatchGesture(gestureDescription, callback, null);
+        }
+        return false;
+    }
+
+    /**
+     * 模拟点击事件
+     *
+     * @param nodeInfo nodeInfo
+     */
+    public void performViewClick(AccessibilityNodeInfo nodeInfo) {
+        if (nodeInfo == null) {
+            return;
+        }
+        while (nodeInfo != null) {
+            if (nodeInfo.isClickable()) {
+                nodeInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                break;
+            }
+            nodeInfo = nodeInfo.getParent();
+        }
+    }
+
+
+    /**
+     * 模拟返回操作
+     */
+    public void performBackClick() {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        performGlobalAction(GLOBAL_ACTION_BACK);
+    }
+
+
+    /**
+     * 查找对应文本的View
+     *
+     * @param text text
+     * @return View
+     */
+    public AccessibilityNodeInfo findViewByText(String text) {
+        return findViewByText(text, false);
+    }
+
+    /**
+     * 查找对应文本的View
+     *
+     * @param text      text
+     * @param clickable 该View是否可以点击
+     * @return View
+     */
+    public AccessibilityNodeInfo findViewByText(String text, boolean clickable) {
+        AccessibilityNodeInfo accessibilityNodeInfo = getRootInActiveWindow();
+        if (accessibilityNodeInfo == null) {
+            return null;
+        }
+        List<AccessibilityNodeInfo> nodeInfoList = accessibilityNodeInfo.findAccessibilityNodeInfosByText(text);
+        if (nodeInfoList != null && !nodeInfoList.isEmpty()) {
+            for (AccessibilityNodeInfo nodeInfo : nodeInfoList) {
+                if (nodeInfo != null && (nodeInfo.isClickable() == clickable)) {
+                    return nodeInfo;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 查找对应文本的View
+     *
+     * @param text      text
+     * @param clickable 该View是否可以点击
+     * @return View
+     */
+    public AccessibilityNodeInfo findViewByText(AccessibilityNodeInfo accessibilityNodeInfo, String text, boolean clickable) {
+        if (accessibilityNodeInfo == null) {
+            return null;
+        }
+        List<AccessibilityNodeInfo> nodeInfoList = accessibilityNodeInfo.findAccessibilityNodeInfosByText(text);
+        if (nodeInfoList != null && !nodeInfoList.isEmpty()) {
+            for (AccessibilityNodeInfo nodeInfo : nodeInfoList) {
+                if (nodeInfo != null && (nodeInfo.isClickable() == clickable)) {
+                    return nodeInfo;
+                }
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * 查找对应文本的View
+     *
+     * @param text text
+     * @return View
+     */
+    public AccessibilityNodeInfo findViewByText(AccessibilityNodeInfo accessibilityNodeInfo, String text) {
+        if (accessibilityNodeInfo == null) {
+            return null;
+        }
+        List<AccessibilityNodeInfo> nodeInfoList = accessibilityNodeInfo.findAccessibilityNodeInfosByText(text);
+        if (nodeInfoList != null && !nodeInfoList.isEmpty()) {
+            for (AccessibilityNodeInfo nodeInfo : nodeInfoList) {
+                if (nodeInfo != null) {
+                    return nodeInfo;
+                }
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * 查找对应ID的View
+     *
+     * @param id id
+     * @return View
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public AccessibilityNodeInfo findViewByID(String id) {
+        AccessibilityNodeInfo accessibilityNodeInfo = getRootInActiveWindow();
+        if (accessibilityNodeInfo == null) {
+            return null;
+        }
+        List<AccessibilityNodeInfo> nodeInfoList = accessibilityNodeInfo.findAccessibilityNodeInfosByViewId(id);
+        if (nodeInfoList != null && !nodeInfoList.isEmpty()) {
+            for (AccessibilityNodeInfo nodeInfo : nodeInfoList) {
+                if (nodeInfo != null) {
+                    return nodeInfo;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 查找对应ID的View
+     *
+     * @param id id
+     * @return View
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public AccessibilityNodeInfo findViewByID(AccessibilityNodeInfo accessibilityNodeInfo, String id) {
+        if (accessibilityNodeInfo == null) {
+            return null;
+        }
+        List<AccessibilityNodeInfo> nodeInfoList = accessibilityNodeInfo.findAccessibilityNodeInfosByViewId(id);
+        if (nodeInfoList != null && !nodeInfoList.isEmpty()) {
+            for (AccessibilityNodeInfo nodeInfo : nodeInfoList) {
+                if (nodeInfo != null) {
+                    return nodeInfo;
+                }
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * 查找对应ID的View
+     *
+     * @param id id
+     * @return View
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public List<AccessibilityNodeInfo> findViewsByID(String id) {
+        AccessibilityNodeInfo accessibilityNodeInfo = getRootInActiveWindow();
+        if (accessibilityNodeInfo == null) {
+            return null;
+        }
+        List<AccessibilityNodeInfo> nodeInfoList = accessibilityNodeInfo.findAccessibilityNodeInfosByViewId(id);
+        return nodeInfoList;
+    }
+
+
+    public void clickTextViewByText(String text) {
+        AccessibilityNodeInfo accessibilityNodeInfo = getRootInActiveWindow();
+        if (accessibilityNodeInfo == null) {
+            return;
+        }
+        List<AccessibilityNodeInfo> nodeInfoList = accessibilityNodeInfo.findAccessibilityNodeInfosByText(text);
+        if (nodeInfoList != null && !nodeInfoList.isEmpty()) {
+            for (AccessibilityNodeInfo nodeInfo : nodeInfoList) {
+                if (nodeInfo != null) {
+                    performViewClick(nodeInfo);
+                    break;
+                }
+            }
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public void clickTextViewByID(String id) {
+        AccessibilityNodeInfo accessibilityNodeInfo = getRootInActiveWindow();
+        if (accessibilityNodeInfo == null) {
+            return;
+        }
+        List<AccessibilityNodeInfo> nodeInfoList = accessibilityNodeInfo.findAccessibilityNodeInfosByViewId(id);
+        if (nodeInfoList != null && !nodeInfoList.isEmpty()) {
+            for (AccessibilityNodeInfo nodeInfo : nodeInfoList) {
+                if (nodeInfo != null) {
+                    performViewClick(nodeInfo);
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * 模拟输入
+     *
+     * @param nodeInfo nodeInfo
+     * @param text     text
+     */
+    public void inputText(AccessibilityNodeInfo nodeInfo, String text) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Bundle arguments = new Bundle();
+            arguments.putCharSequence(AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE, text);
+            nodeInfo.performAction(AccessibilityNodeInfo.ACTION_SET_TEXT, arguments);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("label", text);
+            clipboard.setPrimaryClip(clip);
+            nodeInfo.performAction(AccessibilityNodeInfo.ACTION_FOCUS);
+            nodeInfo.performAction(AccessibilityNodeInfo.ACTION_PASTE);
+        }
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+    }
+}
